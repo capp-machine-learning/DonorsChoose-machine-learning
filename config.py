531d44cdce720002a61761ccec4d36cdf8be2391
Si Young Byun <siyoung.byun@gmail.com>
2019-05-26 01:22:07 -0500

    implementing logger

--- config.py
+++ config.py
@@ -6,17 +6,40 @@ Si Young Byun
 
 import yaml
 import logging
+import os
+import logging.config
+logging.config.fileConfig('log_config.conf')
+LOGGER = logging.getLogger("DonorsML")
 
-logging.basicConfig(filename='./results.log', filemode='w', level=logging.INFO)
 
+def read_config(filename, log=False):
 
-def read_config(filename, verbose=False):
+    if log is True:
+        LOGGER.info("\n# Loading configurations...")
 
     try:
         with open(filename, 'r') as ymlfile:
             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    
+        
+        if log is True:
+            LOGGER.info("    >>> SUCCESS!")
+
         return cfg
     
     except:
+        if log is True:
+            LOGGER.info("    >>> FAILED!")
         
+        return None
+
+
+#-----------------------------------------------------------------------------#
+if __name__ == "__main__":
+    # For debugging purpose only
+    if os.path.isfile("./results.log"):
+        os.remove("./results.log")
+        logging.config.fileConfig('log_config.conf')
+        LOGGER = logging.getLogger("DonorsML")
+        LOGGER.info("# Deleted the existing log file.")
+
+    cfg = read_config("./config.yaml", log=True)
