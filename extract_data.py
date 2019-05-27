5768330bf4ceb531940e628a471f8024fa382774
Si Young Byun <siyoung.byun@gmail.com>
2019-05-27 15:40:42 -0500

    preprocessing fixed

--- extract_data.py
+++ extract_data.py
@@ -25,10 +25,10 @@ def convert_dtypes(df, logger=None):
                 for feature in features:
                     df[feature] = df[feature].astype(dtype)
         
-        log_msg(logger, "S")
+        log_msg(logger, "SUCCESS")
     
     except:
-        log_msg(logger, "F")
+        log_msg(logger, "FAILED")
 
 
 def read_data(filename, convert=False, logger=None):
@@ -47,10 +47,10 @@ def read_data(filename, convert=False, logger=None):
 
         if ext == '.csv':
             df = pd.read_csv(filename, index_col=0)
-            log_msg(logger, "S")
+            log_msg(logger, "SUCCESS")
         elif ext in ['.xls', '.xlsx']:
             df = pd.read_excel(filename, index_col=None)
-            log_msg(logger, "S")
+            log_msg(logger, "SUCCESS")
 
         if convert is True:
             convert_dtypes(df, logger)
@@ -58,7 +58,7 @@ def read_data(filename, convert=False, logger=None):
         return df
 
     except:
-        log_msg(logger, "F")
+        log_msg(logger, "FAILED")
 
 
 # Summarize the loaded data
@@ -67,10 +67,10 @@ def summarize_data(df, logger=None):
     row = df.shape[0]
     col = df.shape[1]
 
-    partition = "###########################################################\n"
+    partition = "\n#########################################################\n"
     log_msg(logger, partition)
     log_msg(logger, "Summary for the loaded dataset\n")
     log_msg(logger, "The total number of rows is {}\n".format(row))
     log_msg(logger, "The total number of rows is {}\n".format(col))
-    log_msg(logger, "Descriptive Statistics:\n{}\n".format(df.describe()))
+    log_msg(logger, "Descriptive Statistics:\n\n{}\n".format(df.describe()))
     log_msg(logger, partition)
--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ Si Young Byun
 '''
 
 import pandas as pd
+#import sys
 
 from config import load_log_config, log_msg, read_config
 from extract_data import *
@@ -28,13 +29,22 @@ def select_features(df, features):
     return df
 
 
-def generate_time_label(df, start, end, label):
+def generate_time_label(df, time_cols, dep_var, logger=None):
 	
-    time_int = df[end] - df[start]
-    df[label] = (time_int <= pd.to_timedelta(60, unit='days')).astype('int')
+    msg = "\n# Attempting to generate the dependent variable based on {}..."
+    log_msg(logger, msg.format(time_cols))
+
+    try:
+        (start, end) = time_cols
+        time_int = df[end] - df[start]
+        df[dep_var] = (time_int <= pd.to_timedelta(60, unit='days')).astype('int')
+        log_msg(logger, "SUCCESS")
+
+    except:
+        log_msg(logger, "FAILED")
 
 
-def generate_dummy(df, features):
+def generate_dummy(df, features, logger=None):
     '''
     Given the dataset and the variable, generate dummy variables for it.
     - df: (pandas dataframe) dataframe of the dataset
@@ -42,15 +52,24 @@ def generate_dummy(df, features):
     Output:
     - Nothing
     '''
-    for feature in features:
-        dummy = pd.get_dummies(df[feature])
-        df = pd.concat([df, dummy], axis=1)
-        df = df.drop(columns=[feature])
+    msg = "\n# For the following variables, dummy variables have been created."
+    log_msg(logger, msg)
+    log_msg(logger, "    >>>{}".format(features))
 
-    return df
+    try:
+        for feature in features:
+            dummy = pd.get_dummies(df[feature])
+            df = pd.concat([df, dummy], axis=1)
+            df = df.drop(columns=[feature])
+        log_msg(logger, "SUCCESS")
+        return df
+    
+    except:
+        log_msg(logger, "FAILED")
+        return None
 
 
-def analyze_missing_data(df):
+def analyze_missing_data(df, logger=None):
     '''
     Find variables with missing data and return those in a list.
     Input:
@@ -58,29 +77,36 @@ def analyze_missing_data(df):
     Output:
     - nan_vars: (list) list of the name of the variables with missing values
     '''
-    nan = df.isna().sum()
-    nan_perc = round(100 * nan / len(df.index), 2)
-    nan_df = pd.concat([nan, nan_perc], axis=1)
-    nan_df = nan_df.rename(columns={0: 'NaN', 1: 'Percent of NaN'})
-    nan_df = nan_df.sort_values(by=['Percent of NaN'], ascending=False)
-    only_nan_df = nan_df[nan_df['NaN'] > 0]
-    nan_vars = only_nan_df.index.tolist()
 
-    print(only_nan_df)
-    print("\n################################################################")
-    print("\nThe following features have missing values:\n{}".format(nan_vars))
+    msg = "\n# Attempting to analyze the missing data..."
+    log_msg(logger, msg)
+    
+    try:
+        log_msg(logger, "SUCCESS")
+        nan = df.isna().sum()
+        nan_perc = round(100 * nan / len(df.index), 2)
+        nan_df = pd.concat([nan, nan_perc], axis=1)
+        nan_df = nan_df.rename(columns={0: 'NaN', 1: 'Percent of NaN'})
+        nan_df = nan_df.sort_values(by=['Percent of NaN'], ascending=False)
+        only_nan_df = nan_df[nan_df['NaN'] > 0]
+        nan_vars = only_nan_df.index.tolist()
+        #logging
+        msg1 = "\nThe following features have missing values:\n    >>>{}\n"
+        log_msg(logger, msg1.format(nan_vars))
+        msg2 = "- {} has {} missing values, which are {}% of the data"
 
-    message = "\n- {} has {} missing values, which are {}% of the entire data"
+        for var in nan_vars:
+            num = only_nan_df.loc[var][0]
+            perc = only_nan_df.loc[var][1]
+            log_msg(logger, msg2.format(var, int(num), perc))
 
-    for var in nan_vars:
-        num = only_nan_df.loc[var][0]
-        perc = only_nan_df.loc[var][1]
-        print(message.format(var, int(num), perc))
+        return nan_vars
 
-    return nan_vars
+    except:
+        log_msg(logger, "FAILED")
 
 
-def impute_missing_data(df, columns):
+def impute_missing_data(df, columns, logger=None):
     '''
     Given the dataset, impute missing data for the given variables.
     - df: (pandas dataframe) dataframe of the dataset
@@ -88,51 +114,53 @@ def impute_missing_data(df, columns):
     Output:
     - Nothing
     '''
-    for column in columns:
-        if df[column].dtype == 'object':
-            estimate = df[column].value_counts().idxmax()
-            print("For {}, the most common value is selected.".format(column))
-        else:
-            if abs(df[column].kurt()) > 3:
-                cond = df[column].median()
-                print("For {}, median is selected.".format(column))
-            else:
-                cond = df[column].mean()
-                print("For {}, mean is selected.".format(column))
-            estimate = round(cond)
-        df[column] = df[column].fillna(estimate)
+    msg = "\n# Attempting to impute the missing data...\n"
+    log_msg(logger, msg)
 
-    print("\nImputation completed!")
+    try:
+        for col in columns:
+            if df[col].dtype == 'object':
+                estimate = df[col].value_counts().idxmax()
+                msg = "- For {}, the most common value is selected."
+            else:
+                if abs(df[col].kurt()) > 3:
+                    cond = df[col].median()
+                    msg = "- For {}, median is selected."
+                else:
+                    cond = df[col].mean()
+                    msg = "- For {}, mean is selected."
+                estimate = round(cond)
+            log_msg(logger, msg.format(col))
+            df[col] = df[col].fillna(estimate)
+        
+        log_msg(logger, "SUCCESS")
+    
+    except:
+        log_msg(logger, "FAILED")
 
 
 #-----------------------------------------------------------------------------#
 if __name__ == "__main__":
 
-    try:
-        df = read_data(DATA_DIR + DATAFILE)
-        print("\nThe data is successfully loaded!\n")
-        summarize_data(df)
-        print("ATTEMPTING TO PREPROCESS THE DATA\n")
-
-        try:
-            clean_name = "clean_" + DATAFILE
-            directory = DATA_DIR + clean_name
-            df = select_features(df, FEATURES)
-            print("The following features have been selected:\n{}".format(FEATURES))
-            generate_time_label(df, START, END, OUTCOME)
-            print("\nThe label is appended as {}.".format(OUTCOME))
-            df = generate_dummy(df, CAT)
-            print("Dummy variables are created based on {}.".format(CAT))
-            nan_vars = analyze_missing_data(df)
-            impute_missing_data(df, nan_vars)
-            df = df.reset_index()
-            df = df.drop(['projectid', 'datefullyfunded'], axis=1)
-            df.to_csv(DATA_DIR + clean_name)
-            print("The cleaned data is saved as {}.\n".format(directory))
-
-        except:
-            print("Failed to clean and save the dataset.\n")
-
-    except:
-        print("Failed to read the data. Please check the filename.")
+    if os.path.isfile("./results.log"):
+        os.remove("./results.log")
     
+    LOGGER = load_log_config()
+    log_msg(LOGGER, "# Deleted the existing log file.")
+
+    df = read_data(DATA_DIR + DATAFILE, convert=True, logger=LOGGER)
+    summarize_data(df, logger=LOGGER)
+
+    clean_name = "clean_" + DATAFILE
+    directory = DATA_DIR + clean_name
+    generate_time_label(df, [START, END], OUTCOME, LOGGER)
+    df = generate_dummy(df, CAT, LOGGER)
+
+    #Missing Data and Imputation
+    nan_vars = analyze_missing_data(df, LOGGER)
+    impute_missing_data(df, nan_vars, LOGGER)
+
+    df = df.reset_index()
+    df = df.drop(['projectid', 'datefullyfunded'], axis=1)
+    df.to_csv(DATA_DIR + clean_name)
+    log_msg(LOGGER, "\nThe cleaned data is saved as {}.\n".format(directory))
